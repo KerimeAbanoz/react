8401225f86900d6e39df4c426f956fc8d67cb1a5
kerime abanoz <krmeabnz@gmail.com>
2022-11-03 14:52:58 +0300

    break

--- rc16_context-api/context-example1/context-example1-inClass/src/pages/Login.jsx
+++ rc16_context-api/context-example1/context-example1-inClass/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { LoginContext } from "../context/LoginContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   //! local state
@@ -11,8 +12,10 @@ const Login = () => {
   //! Consuming LoginContext
   const { user, setUser } = useContext(LoginContext);
 
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
+    navigate(-1);
   };
 
   console.log(user);
