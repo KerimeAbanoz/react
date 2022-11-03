f98151a6e942dbf9c81856e62dd7acae2dcc8730
kerime abanoz <krmeabnz@gmail.com>
2022-11-03 20:52:29 +0300

    break

--- rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
+++ rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
@@ -1,5 +1,39 @@
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
 const TypoButtons = () => {
-  return <div>TypoButtons</div>;
+  return (
+    <Container>
+      <Typography
+        variant="h4"
+        component="h6"
+        color="error"
+        align="center"
+        mt={4}
+      >
+        MUI TYPOGRAPHY
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textShadow: " 1px 1px 3px yellow" }}
+        align="justify"
+        mt={4}
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur,
+        aspernatur vitae fugit maiores ea debitis quod, illo veritatis
+        consequuntur laboriosam, eos consequatur? Natus quod perferendis
+        reprehenderit vitae, nemo cupiditate facilis.
+      </Typography>
+
+      <Box display="flex" width="50%" flexDirection="column" gap={2} mt={2}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default TypoButtons;
