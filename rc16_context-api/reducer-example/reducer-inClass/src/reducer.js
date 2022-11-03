0007b894e9a5a014cec0d72a59bfbe87f66b8d35
kerime abanoz <krmeabnz@gmail.com>
2022-11-03 17:58:49 +0300

    all done

--- rc16_context-api/reducer-example/reducer-inClass/src/reducer.js
+++ rc16_context-api/reducer-example/reducer-inClass/src/reducer.js
@@ -1,19 +1,21 @@
 export const initialState = {
-  loading: false,
-  catImage: "",
-  error: "",
-};
-
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case "START":
-      return { ...state, loadng: true };
-    case "SUCCESS":
-      return { ...state, catImage: action.payload, error: "", loadng: false };
-    case "FAIL":
-      return { ...state, catImage: "", error: action.payload, loadng: false };
-    default:
-      break;
-  }
-  return {};
-};
+    loading: false,
+    catImage: "",
+    error: "",
+  };
+  
+  export const reducer = (state, action) => {
+    switch (action.type) {
+      case "START":
+        return { ...state, loading: true };
+  
+      case "SUCCESS":
+        return { ...state, catImage: action.payload, error: "", loading: false };
+  
+      case "FAIL":
+        return { ...state, catImage: "", error: action.payload, loading: false };
+      default:
+        break;
+    }
+  };
+  
