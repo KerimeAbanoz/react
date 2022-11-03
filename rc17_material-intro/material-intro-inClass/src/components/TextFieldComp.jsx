5b56eb739d09369875fbbacba3a473b19e04fd1a
kerime abanoz <krmeabnz@gmail.com>
2022-11-03 21:49:56 +0300

    card

--- rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
+++ rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
@@ -2,21 +2,45 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material";
-
-import React from 'react'
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 const TextFieldComp = () => {
+  const [error, setError] = useState(false);
   return (
     <div>
-        <Container>
-            <Typography variant="h4" color="error" align="center" mt={4}>
-                TEXT FIELD
-            </Typography>
+      <Container>
+        <Typography variant="h4" color="error" align="center" mt={4}>
+          TEXT FIELD
+        </Typography>
 
-        </Container>
+        <Box sx={{ mt: 4, textAlign: "center" }}>
+          <TextField
+            margin="normal"
+            id="email"
+            label="Email"
+            placeholder="Enter your email"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect email format"}
+          />
+          <TextField
+            margin="normal"
+            id="password"
+            label="Password"
+            placeholder="Enter your password"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect password"}
+          />
+
+          <Button variant="contained" color="warning" sx={{ mt: 4 }}>
+            Submit
+          </Button>
+        </Box>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default TextFieldComp
+export default TextFieldComp;
