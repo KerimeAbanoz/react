1c72acaeee23459dd55e329918f6074718dcb67b
kerime abanoz <krmeabnz@gmail.com>
2022-11-03 21:02:33 +0300

    break

---
+++ rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
@@ -0,0 +1,22 @@
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material";
+
+import React from 'react'
+
+const TextFieldComp = () => {
+  return (
+    <div>
+        <Container>
+            <Typography variant="h4" color="error" align="center" mt={4}>
+                TEXT FIELD
+            </Typography>
+
+        </Container>
+    </div>
+  )
+}
+
+export default TextFieldComp
--- rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
+++ rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
@@ -2,37 +2,72 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
 const TypoButtons = () => {
   return (
-    <Container>
-      <Typography
-        variant="h4"
-        component="h6"
-        color="error"
-        align="center"
-        mt={4}
-      >
-        MUI TYPOGRAPHY
-      </Typography>
-      <Typography
-        variant="body1"
-        sx={{ textShadow: " 1px 1px 3px yellow" }}
-        align="justify"
-        mt={4}
-      >
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur,
-        aspernatur vitae fugit maiores ea debitis quod, illo veritatis
-        consequuntur laboriosam, eos consequatur? Natus quod perferendis
-        reprehenderit vitae, nemo cupiditate facilis.
-      </Typography>
+    <>
+      <Container maxWidth="md">
+        <Typography
+          variant="h4"
+          component="h6"
+          align="center"
+          mt={4}
+          color="error"
+        >
+          MUI Typography
+        </Typography>
 
-      <Box display="flex" width="50%" flexDirection="column" gap={2} mt={2}>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Box>
-    </Container>
+        <Typography
+          variant="body2"
+          align="justify"
+          mt={4}
+          sx={{
+            backgroundColor: "lightgreen",
+            color: "#eee",
+            fontSize: "1.4rem",
+          }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          aspernatur et placeat ab aliquid, consectetur perspiciatis numquam
+          voluptates nulla quisquam!
+        </Typography>
+
+        <Typography
+          variant="button"
+          sx={{ display: "inline-block", marginTop: "2rem" }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          debitis quo omnis porro labore a consequuntur beatae quam assumenda
+          dicta explicabo nam illo nemo, provident, veniam aliquid aut tenetur
+          accusamus?
+        </Typography>
+      </Container>
+
+      <Container>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.5rem",
+            // width: "50%",
+            // justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+          <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+          <Button variant="contained" color="secondary" endIcon={<SendIcon />}>
+            Send
+          </Button>
+        </Box>
+      </Container>
+    </>
   );
 };
 
