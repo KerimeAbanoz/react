9472ce4160a3a0db7a5b5bff7717085f4ae89f4a
kerime abanoz <krmeabnz@gmail.com>
2023-01-12 12:51:09 +0300

    progress

---
+++ rc16_context-api/context-example1/context-example1-starter/src/context/LoginContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+//! 1-) Creating login context
+export const LoginContext = createContext();
--- rc16_context-api/context-example1/context-example1-starter/src/pages/Login.jsx
+++ rc16_context-api/context-example1/context-example1-starter/src/pages/Login.jsx
@@ -2,14 +2,21 @@ import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 const Login = () => {
-  const [user, setUser] = useState({ email: "", password: "" });
+  // ! local state
+  // const [user, setUser] = useState({ email: "", password: "" });
+
+  //! Consuming login context
+  const { user, setUser } = useContext(LoginContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
+  console.log(user);
   return (
     <Container>
       <h1 className="text-center mt-4">LOGIN PAGE</h1>
@@ -20,7 +27,7 @@ const Login = () => {
             type="email"
             placeholder="Enter your email"
             name="email"
-            value={user?.email}
+            value={user?.email || ""}
             onChange={(e) => setUser({ ...user, email: e.target.value })}
           />
         </Form.Group>
@@ -31,7 +38,7 @@ const Login = () => {
             type="password"
             placeholder="Enter your password"
             name="password"
-            value={user?.password}
+            value={user?.password || ""}
             onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
         </Form.Group>
