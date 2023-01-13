f56d562c37b632be71a8bfe88af37c6a43c03e93
kerime abanoz <krmeabnz@gmail.com>
2023-01-13 17:52:52 +0300

    card

--- rc17_material-intro/material-intro-starter/src/pages/Home.jsx
+++ rc17_material-intro/material-intro-starter/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import TypoButtons from "../components/TypoButtons";
-
+import TextFieldComp from "../components/TextFieldComp";
+import CardGrid from "../components/CardGrid";
 const Home = () => {
   return (
     <>
       <TypoButtons />
+      <TextFieldComp />
+      <CardGrid/>
     </>
   );
 };
