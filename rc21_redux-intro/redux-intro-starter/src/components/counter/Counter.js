fae9a105e23f71cae20e673881662099c41cf24c
kerime abanoz <krmeabnz@gmail.com>
2023-01-24 18:04:13 +0300

    counter structured

--- rc21_redux-intro/redux-intro-starter/src/components/counter/Counter.js
+++ rc21_redux-intro/redux-intro-starter/src/components/counter/Counter.js
@@ -1,9 +1,16 @@
 import "./Counter.css";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  clear,
+  decrement,
+} from "../../redux/actions/counterActions";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  // const counter = useSelector((state) => state.count.counter);
+  const { counter } = useSelector((state) => state.count);
+  // const {todoList} = useSelector((state) => state.todo);
 
   return (
     <div className="app">
@@ -12,19 +19,22 @@ const Counter = () => {
       <div>
         <button
           className="counter-button positive"
-          onClick={() => dispatch({ type: "INCREMENT" })}
+          // onClick={() => dispatch({ type: "INCREMENT" })}
+          onClick={() => dispatch(increment())}
         >
           increase
         </button>
         <button
           className="counter-button zero"
-          onClick={() => dispatch({ type: "CLEAR" })}
+          // onClick={() => dispatch({ type: "CLEAR" })}
+          onClick={() => dispatch(clear())}
         >
           reset
         </button>
         <button
           className="counter-button negative"
-          onClick={() => dispatch({ type: "DECREMENT" })}
+          // onClick={() => dispatch({ type: "DECREMENT" })}
+          onClick={() => dispatch(decrement())}
         >
           decrease
         </button>
